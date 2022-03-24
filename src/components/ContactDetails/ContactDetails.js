6970a83fbd5270326e4b48b921307229d3d6abdd
Christopher Hauser <chauser6@gmail.com>
2022-03-24 19:09:20 -0400

    restyling

--- src/components/ContactDetails/ContactDetails.js
+++ src/components/ContactDetails/ContactDetails.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function ContactDetails( { contact } ) {
+function ContactDetails({ contact }) {
 
     return (
         <div className="contact-details-block">
@@ -10,13 +10,22 @@ function ContactDetails( { contact } ) {
 
             {contact && (
                 <>
-                    <img src={contact.picture.large} alt={`${contact.name.first}-${contact.name.last}`} className='contact-photo' />
-                    <h3>{`${contact.name.first} ${contact.name.last}`}</h3>
-                    <p>{`${contact.phone}`}</p>
-                    <div className="address-info">
-                        <p>{`${contact.location.street.number} ${contact.location.street.name}`}</p>
-                        <p>{`${contact.location.city}, ${contact.location.state}`}</p>
-                        <p>{`${contact.location.country}`}</p>
+                    <div className="contact-details-top">
+                        <img src={contact.picture.large} alt={`${contact.name.first}-${contact.name.last}`} className='contact-photo' />
+                        <h3>{`${contact.name.first} ${contact.name.last}`}</h3>
+                    </div>
+                    <div className="contact-details-bottom">
+                        <div className="contact-detail-block">
+                            <p>{`${contact.cell}`}</p>
+                        </div>
+                        <div className="contact-detail-block">
+                            <p>{`${contact.phone}`}</p>
+                        </div>
+                        <div className="contact-detail-block">
+                            <p>{`${contact.location.street.number} ${contact.location.street.name}`}</p>
+                            <p>{`${contact.location.city}, ${contact.location.state}`}</p>
+                            <p>{`${contact.location.country}`}</p>
+                        </div>
                     </div>
                 </>
             )}
