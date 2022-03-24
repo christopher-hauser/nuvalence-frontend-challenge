d5d39be61556be5512c57d6f72bac0cdb5887530
Christopher Hauser <chauser6@gmail.com>
2022-03-24 15:10:57 -0400

    additional simple styling on details card

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -13,9 +13,11 @@ function ContactDetails( { contact } ) {
                     <img src={contact.picture.large} alt={`${contact.name.first}-${contact.name.last}`} className='contact-photo' />
                     <h3>{`${contact.name.first} ${contact.name.last}`}</h3>
                     <p>{`${contact.phone}`}</p>
-                    <p>{`${contact.location.street.number} ${contact.location.street.name}`}</p>
-                    <p>{`${contact.location.city}, ${contact.location.state}`}</p>
-                    <p>{`${contact.location.country}`}</p>
+                    <div className="address-info">
+                        <p>{`${contact.location.street.number} ${contact.location.street.name}`}</p>
+                        <p>{`${contact.location.city}, ${contact.location.state}`}</p>
+                        <p>{`${contact.location.country}`}</p>
+                    </div>
                 </>
             )}
         </div>
