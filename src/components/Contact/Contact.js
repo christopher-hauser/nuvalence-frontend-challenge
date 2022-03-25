298869545154ee3ef046e59607578babe9e170dd
Christopher Hauser <chauser6@gmail.com>
2022-03-25 14:53:44 -0400

    added selected block styling

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectContact } from '../../store/contacts'
 
 function Contact({ contact }) {
     const dispatch = useDispatch();
+    const selected = useSelector(state => state.contacts.selectedContact)
+    const isSelected = contact.name.last === selected.name?.last && contact.name.first === selected?.name.first;
 
     const handleClick = () => {
         dispatch(selectContact(contact))
@@ -11,7 +13,7 @@ function Contact({ contact }) {
 
     return (
         <>
-            <div className="contact-block"
+            <div className={isSelected ? "contact-block selected" : "contact-block"}
                 onClick={handleClick}>
                 <h4 data-testid='contact-name'>{`${contact?.name.first} ${contact.name.last}`}</h4>
                 <p data-testid='contact-location'>{`${contact?.location.city}, ${contact.location.country}`}</p>
