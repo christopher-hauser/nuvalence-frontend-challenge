caaff70b23c5f9b2358eddb83d785d5c83c328f7
Christopher Hauser <chauser6@gmail.com>
2022-03-28 22:27:39 -0400

    package.json updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import AddressBook from './components/AddressBook/AddressBook';
 import Nav from './components/Nav/Nav';
 import './styles.scss'
@@ -7,10 +6,10 @@ import './styles.scss'
 function App() {
 
     return (
-        <BrowserRouter>
+        <>
             <Nav />
             <AddressBook />
-        </BrowserRouter>
+        </>
     )
 }
 
